afd20c19a559870b8da2b5d61cc5e9fe5b458b54
nwtas <115685597+nwtas@users.noreply.github.com>
2023-07-29 11:39:55 +1000

    timer update

--- monitor_pins.py
+++ monitor_pins.py
@@ -81,8 +81,8 @@ def Main():
 
             # If the track timers have not run for 10 minutes, hide pygame window
             if display_active == True and (
-                track_timer_1.get_time_elapsed_from_last_start() > 600
-                or track_timer_2.get_time_elapsed_from_last_start() > 600
+                track_timer_1.get_time_elapsed_from_last_start() > 60
+                or track_timer_2.get_time_elapsed_from_last_start() > 60
             ):
                 pygame.display.set_mode((1, 1))
                 display_active = False
