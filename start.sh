2604297cdb1ba6c0b504b18413c7840b771a9a27
nwtas <115685597+nwtas@users.noreply.github.com>
2024-11-01 15:03:53 +1100

    creating log file

--- start.sh
+++ start.sh
@@ -4,9 +4,12 @@
 # So now I've put it in a loop to restart the application when it fails.
 cd /home/hive/Scripts
 
+# Move the log file so that we don't fill up all space.
+mv /home/hive/Scripts/monitor_pins.log /home/hive/Scripts/monitor_pins.log.old
+
 while true
 do
-    ./monitor_pins.py
+    ./monitor_pins.py >> /home/hive/Scripts/monitor_pins.log 2>&1
     echo "Script stopped! waiting 30 seconds then trying again"
     # Sleep 20 seconds to give time for everything to shutdown and clean up
     # AND to give time for a normal exit by operator to take control before restart.
